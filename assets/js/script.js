63160cdb8af40e6758921bea2d76172fa39dd154
leham789 <leham789@hotmail.co.uk>
2018-02-05 04:28:14 +0000

    Location Animations Scroll Reveal Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,23 +9,21 @@ $(function(){
         var imgURL = $img.attr('src');
 
         jQuery.get(imgURL, function(data) {
-            // Get the SVG tag, ignore the rest
+
             var $svg = jQuery(data).find('svg');
-            // Add replaced image's ID to the new SVG
+
             if(typeof imgID !== 'undefined') {
                 $svg = $svg.attr('id', imgID);
             }
-            // Add replaced image's classes to the new SVG
+
             if(typeof imgClass !== 'undefined') {
                 $svg = $svg.attr('class', imgClass+' replaced-svg');
             }
-            // Remove any invalid XML tags as per http://validator.w3.org
+
             $svg = $svg.removeAttr('xmlns:a');
-            // Check if the viewport is set, else we gonna set it if we can.
             if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
                 $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
             }
-            // Replace image with new SVG
             $img.replaceWith($svg);
         }, 'xml');
     });
@@ -40,3 +38,13 @@ $(function(){
         document.getElementById("deleteUserPhotoInput").value = 'zap';
     });
 })();
+
+
+//Scroll Reveal
+window.sr = new ScrollReveal();
+sr.reveal('.locations', {
+  reset: true,
+  duration: 1100,
+  origin: 'bottom',
+  distance: '20px',
+}, 0);
