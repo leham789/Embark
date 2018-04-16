ff99c8dcdfb479f80db7ece9ef47e9edd1aa3198
leham789 <leham789@hotmail.co.uk>
2018-04-16 17:21:29 +0100

    links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,5 @@
 //Script
 
-// Mobile Safari in standalone mode
-if (('standalone' in window.navigator) && window.navigator.standalone) {
-
-    window.addEventListener('load', function() {
-
-        var links = document.links,
-            link,
-            i;
-
-        for (i = 0; i < links.length; i++) {
-            // Don't do this for javascript: links
-            if (~(link = links[i]).href.toLowerCase().indexOf('javascript')) {
-                link.addEventListener('click', function(event) {
-                    top.location.href = this.href;
-                    event.returnValue = false;
-                }, false);
-            }
-        }
-
-    }, false);
-
-}
-
 //SVG Image to Outline
 $(function(){
     jQuery('img.svg').each(function(){
